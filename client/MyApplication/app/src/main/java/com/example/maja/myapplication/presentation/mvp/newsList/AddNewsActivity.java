1b2ade79c17ee84b6aa69f860cc8fdc3627b9c85
ra28 <analukic.ns@gmail.com>
2017-09-04 22:59:45 +0200

    Click event for addNews button is created.

---
+++ client/MyApplication/app/src/main/java/com/example/maja/myapplication/presentation/mvp/newsList/AddNewsActivity.java
@@ -0,0 +1,15 @@
+package com.example.maja.myapplication.presentation.mvp.newsList;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.maja.myapplication.R;
+
+public class AddNewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_news);
+    }
+}
